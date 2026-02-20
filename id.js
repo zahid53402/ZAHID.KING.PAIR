14a84a0326b68799281a42d2c0f66da7bbe8974c
zahid53402 <zahid.ali.00347@gmail.com>
2026-02-20 16:23:42 +0500

    Delete id.js

--- id.js
+++
@@ -1,9 +0,0 @@
-function makeid(num = 4) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
-module.exports = { makeid };
